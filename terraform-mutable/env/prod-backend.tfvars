c8b6c36080a933675368e8b6aef9688e8d28038e
Munish Lagoo <munishlagoo@gmail.com>
2021-12-07 22:10:15 -0500

    add appmodule to component

---
+++ terraform-mutable/env/prod-backend.tfvars
@@ -0,0 +1,3 @@
+bucket= "test-terraform-us-east-1"
+key= "test-Terraform-mutable/cart/${var.ENV}/terraform.tfstate"
+region= "us-east-1"
---
+++ terraform-mutable/vars.tf
@@ -0,0 +1,13 @@
+variable "ENV" {}
+variable "OD_INSTANCE_COUNT" {}
+variable "SPOT_INSTANCE_COUNT" {}
+variable "INSTANCE_TYPE" {}
+variable "APP_VERSION" {
+    default = ""
+}
+variable "NEXUS_USER" {
+    default = ""
+}
+variable "NEXUS_PASS" {
+    default = ""
+}
